fbf9dac728cbb55dfed83c26bdc1ccc8c9a4a2c1
maryanseg <master.drcd@hotmail.com>
2023-05-23 18:31:15 -0500

    clase calculator

--- app/calc.py
+++ app/calc.py
@@ -17,10 +17,10 @@ class Calculator:
     def multiply(self, x, y):
         # if not app.util.validate_permissions(f"{x} * {y}", "user1"):
         #    raise InvalidPermissions('User has no permissions')
+        self.check_types(x, y)
         if y == 0:
             raise TypeError("Multiplication by zero is not possible")
-
-        self.check_types(x, y)
+        
         return x * y
 
     def divide(self, x, y):
