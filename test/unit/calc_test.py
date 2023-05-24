6596f16d6ad91288b8f307c921fca697d972cde4
maryanseg <master.drcd@hotmail.com>
2023-05-23 19:13:45 -0500

    clase cal_test

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch
-import pytest
+
+#import pytest
 
 from app.calc import Calculator
 
@@ -9,7 +10,8 @@ def mocked_validation(*args, **kwargs):
     return True
 
 
-@pytest.mark.unit
+#@pytest.mark.unit
+@unittest.mark.unit
 class TestCalculate(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
